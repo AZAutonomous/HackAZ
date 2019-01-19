7025bd80ac61a80547fb17b6987293b849487ab9
Benjy <timothybduggan@email.arizona.edu>
2019-01-19 12:10:36 -0700

    Overloading intersect - still broken

--- Obstacle_Avoidance/src/Bodies/Map.java
+++ Obstacle_Avoidance/src/Bodies/Map.java
@@ -11,7 +11,7 @@ public class Map {
 	
 	public Map() {
 		obstacles = new ArrayList<Obstacle>();
-		obstacles.add(new Obstacle(new Coordinate(0,0), new Vector(1,0), 1, 1));
+		obstacles.add(new Obstacle(new Coordinate(0,0), new Vector(1,0)));
 		//obstacles.add(new Plane(new Coordinate(5,5), new Vector(0,-1)));
 	}
 	
--- Obstacle_Avoidance/src/Geometry/Line.java
+++ Obstacle_Avoidance/src/Geometry/Line.java
@@ -56,37 +56,15 @@ public class Line {
 		}
 	}
 	
-	public static boolean intersect(Line a, Line b) {
-		return intersect(a.getStart(), a.getEnd(), b.getStart(), b.getEnd());
-	}
-	
 	public static boolean intersect(Coordinate p1, Coordinate p2, Coordinate q1, Coordinate q2) {
-		int o1 = orientation(p1, q1, p2);
-		int o2 = orientation(p1, q1, q2);
-		int o3 = orientation(p2, q2, p1);
-		int o4 = orientation(p2, q2, q1);
 		
-		if (o1 != o2 && o3 != o4) {
-			return true;
-		}
 		
-		if (o1 == 0 && onSegment(p1, p2, q1)) {
-			return true;
-		}
-		
-		if (o2 == 0 && onSegment(p1, q2, q1)) {
-			return true;
-		}
-		
-		if (o3 == 0 && onSegment(p2, p1, q2)) {
-			return true;
-		}
-		
-		if (o4 == 0 && onSegment(p2, q1, q2)) {
-			return true;
-		}
 		
 		return false;
+	}
+	
+	public static boolean intersect(Line a, Line b) {
+		return intersect(a.getStart(), a.getEnd(), b.getStart(), b.getEnd());
 	}
 	
 }
