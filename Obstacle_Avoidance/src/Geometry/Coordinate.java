c1ffd797ccfe4144d11c9e604ffc11bb48e99d9a
Benjy <timothybduggan@email.arizona.edu>
2019-01-18 23:06:33 -0700

    VECTOR AND COORDINATE

--- Obstacle_Avoidance/src/Geometry/Coordinate.java
+++ Obstacle_Avoidance/src/Geometry/Coordinate.java
@@ -1,5 +1,35 @@
 package Geometry;
 
 public class Coordinate {
-
+	private double x, y, z;
+	
+	public Coordinate(double x, double y, double z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	
+	public Coordinate add(Vector that) {
+		return new Coordinate(this.x + that.x, this.y + that.y, this.z + that.z);
+	}
+	
+	public Coordinate subtract(Vector that) {
+		return new Coordinate(this.x - that.x, this.y - that.y, this.z - that.z);
+	}
+	
+	public boolean longitudeSort(Coordinate that) {	
+		if (this.x == that.x) { // if the longitude is the same
+			return (this.y < that.y); // the one with the smaller latitude is less
+		} else {
+			return (this.x < that.x); // the smallest longitude is the smaller one
+		}
+	}
+	
+	public boolean latitudeSort(Coordinate that) {
+		if (this.y == that.y) { // if latitude is the same
+			return (this.x < that.x); // the one with the smaller longitude is less than the other
+		} else {
+			return (this.y < that.y); // the smallest latitude is the smaller one
+		}
+	}
 }
---
+++ Obstacle_Avoidance/src/Geometry/Vector.java
@@ -0,0 +1,80 @@
+package Geometry;
+
+public class Vector {
+	private double x, y, magnitude;
+	
+	public Vector(double x, double y) {
+		this.x = x;
+		this.y = y;
+		this.calculateMagnitude();
+	}
+	
+	public Vector() {
+		this(0,0);
+	}
+	
+	public Vector add(Vector that) {
+		return new Vector(this.x + that.x, this.y + that.y);
+	}
+	
+	public Vector subtract(Vector that) {
+		return new Vector(this.x - that.x, this.y - that.y);
+	}
+	
+	private void calculateMagnitude() {
+		this.magnitude = Math.sqrt(Math.pow(this.x,2) + Math.pow(this.y,2));
+	}
+	
+	public void setX(double x) {
+		this.x = x;
+		this.calculateMagnitude();
+	}
+
+	public double getX() {
+		return this.x;
+	}
+	
+	public void setY(double y) {
+		this.y = y;
+		this.calculateMagnitude();
+	}
+	
+	public double getY() {
+		return this.y;
+	}
+	
+	public Vector scale(double mult) {
+		return new Vector(this.x * mult, this.y * mult);
+	}
+	
+	public double getDirection() {
+		if (this.x > 0) {
+			return Math.atan(this.y / this.x);
+		} else if (this.x < 0) {
+			return Math.atan(this.y / this.x) + Math.PI;
+		} else if (this.x == 0) {
+			if (this.y > 0) {
+				return Math.PI / 2;
+			} else if (this.y < 0) {
+				return -1 * Math.PI / 2;
+			} else if (this.y == 0) {
+				return 0;
+			}
+		}
+		
+		return 0;
+	}
+	
+	public double dotProduct(Vector that) {
+		return (this.x * that.x) + (this.y * that.y); 
+	}
+	
+	public double determinant(Vector that) {
+		return (this.x * that.y)- (this.y * that.x); 
+	}
+	
+	public double angleBetween(Vector that) {
+		double result, dot, deter;
+		dot = this.dotProduct(that);
+	}
+}
