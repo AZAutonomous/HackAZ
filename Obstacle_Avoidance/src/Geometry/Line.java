4e4cd1b0d4feec73b4f4c0f42e194db82ffd32ca
Benjy <timothybduggan@email.arizona.edu>
2019-01-19 11:58:46 -0700

    Line Update

--- Obstacle_Avoidance/src/Geometry/Line.java
+++ Obstacle_Avoidance/src/Geometry/Line.java
@@ -2,10 +2,12 @@ package Geometry;
 
 public class Line {
 	private Coordinate a, b;
+	private double length;
 	
 	public Line(Coordinate a, Coordinate b) {
 		this.a = a;
 		this.b = b;
+		this.calculateLength();
 	}
 	
 	public Line(double x1, double y1, double x2, double y2) {
@@ -13,9 +15,20 @@ public class Line {
 		this.b = new Coordinate(x2, y2);
 	}
 	
-	public static boolean onSegment(Coordinate p, Coordinate q, Coordinate r) {
-		if (q.getX() <= Math.max(p.getX(), q.getX()) && q.getX() >= Math.min(p.getX(), r.getX()) &&
-				q.getY() <= Math.max(p.getY(), q.getY()) && q.getY() >= Math.min(p.getY(), r.getY())) {
+	private void calculateLength() {
+		this.length = Math.sqrt(Math.pow(this.a.getX() - this.b.getX(), 2) + Math.pow(this.a.getY() - this.b.getY(), 2));
+	}
+	
+	public double getLength() {
+		return this.length;
+	}
+	
+	public static double distanceBetween(Coordinate a, Coordinate b) {
+		return Math.sqrt(Math.pow(a.getX() - b.getX(), 2) + Math.pow(a.getY() - b.getY(), 2));
+	}
+	
+	public static boolean onSegment(Coordinate a, Coordinate b, Coordinate c) { // return true if b is on segment ac
+		if (distanceBetween(a,c) == distanceBetween(a,b) + distanceBetween(b,c)) {
 			return true;
 		} else {
 			return false;
@@ -43,7 +56,11 @@ public class Line {
 		}
 	}
 	
-	public boolean intersect(Coordinate p1, Coordinate p2, Coordinate q1, Coordinate q2) {
+	public static boolean intersect(Line a, Line b) {
+		return intersect(a.getStart(), a.getEnd(), b.getStart(), b.getEnd());
+	}
+	
+	public static boolean intersect(Coordinate p1, Coordinate p2, Coordinate q1, Coordinate q2) {
 		int o1 = orientation(p1, q1, p2);
 		int o2 = orientation(p1, q1, q2);
 		int o3 = orientation(p2, q2, p1);
--- Obstacle_Avoidance/src/Tests/LineTests.java
+++ Obstacle_Avoidance/src/Tests/LineTests.java
@@ -1,5 +1,45 @@
 package Tests;
 
-public class LineTests {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Test;
+
+import Geometry.Coordinate;
+import Geometry.Line;
+
+public class LineTests {
+	
+	@Test
+	public void endpointsTest() {
+		Coordinate a = new Coordinate(0,0);
+		Coordinate b = new Coordinate(3,4);
+		Line l1 = new Line(a, b);
+		
+		assertEquals(l1.getLength(), 5, 0.001);
+		
+		Coordinate c = new Coordinate(6,8);
+		Line l2 = new Line(a, c);
+		assertTrue(Line.onSegment(a, b, c));
+		assertFalse(Line.onSegment(a, c, b));
+		
+		assertEquals(l1.getStart().getX(), 0, 0.001);
+		assertEquals(l1.getStart().getY(), 0, 0.001);
+		assertEquals(l1.getEnd().getX(), 3, 0.001);
+		assertEquals(l1.getEnd().getY(), 4, 0.001);
+	}
+	
+	@Test
+	public void orientationTests() {
+		Coordinate a = new Coordinate(0,0);
+		Coordinate b = new Coordinate(1,1);
+		Coordinate c = new Coordinate(1,0);
+		Coordinate d = new Coordinate(0,1);
+		
+		Line l1 = new Line(a,b);
+		Line l2 = new Line(c,d);
+		
+		assertTrue(Line.intersect(l1, l2));
+	}
 }
