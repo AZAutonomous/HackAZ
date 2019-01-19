9d4a343acc5fa88ab9b226bb6ef91795e0ddac78
Jennifer Nadolski <nadolskj@gmail.com>
2019-01-19 02:54:48 -0700

    good commit message

--- Obstacle_Avoidance/src/Main/ObstacleThread.java
+++ Obstacle_Avoidance/src/Main/ObstacleThread.java
@@ -1,8 +1,12 @@
 package Main;
 
+import java.io.*;
 import java.util.ArrayList;
-
+import java.io.FileReader;
+import java.io.BufferedReader;
 import Bodies.Obstacle;
+
+import javax.imageio.IIOException;
 
 public class ObstacleThread implements Runnable{
 
@@ -21,8 +25,35 @@ public class ObstacleThread implements Runnable{
 	//returns 0 if success
 	//else
 	public int updateObstacleList(ArrayList<Obstacle> obstacleList) {
-		
-		
+
+		BufferedReader br = null;
+		FileReader fr = null;
+		File file = new File("obstacle.txt");
+
+		try {
+			fr = new FileReader(file);
+			br = new BufferedReader(fr);
+		}
+		catch (FileNotFoundException f){
+			System.out.println("no");
+		}
+
+		ArrayList<Obstacle> newlist = new ArrayList<Obstacle>();
+
+		Obstacle temp = new Obstacle;
+		try {
+			String x = " ";
+			while ((x = br.readLine()) != null) {
+
+			}
+		}
+		catch (IOException e){
+			System.out.println("noo");
+		}
+
+
+
+		obstacleList = newlist;
 		
 		return 0;
 	}
