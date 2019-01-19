db255dd88feb6bddf0348b45835b4435ccda5add
jamesmurphy7 <jamesmurphy@email.arizona.edu>
2019-01-19 01:56:26 -0700

    here ya go jen

---
+++ Obstacle_Avoidance/src/Main/ObstacleThread.java
@@ -0,0 +1,30 @@
+package Main;
+
+import java.util.ArrayList;
+
+import Bodies.Obstacle;
+
+public class ObstacleThread implements Runnable{
+
+	Obstacle obstacle;
+	
+	//constructor
+	public ObstacleThread(Obstacle o) {
+		this.obstacle = o;
+	}
+	
+	@Override
+	public void run() {
+		
+		
+	}
+	//returns 0 if success
+	//else
+	public int updateObstacleList(ArrayList<Obstacle> obstacleList) {
+		
+		
+		
+		return 0;
+	}
+
+}
